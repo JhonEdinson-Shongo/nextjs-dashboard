5f4719c47a49a107b5d0899c389a3806c85bcc06
JhonEdinson-Shongo <jeca010198@gmail.com>
2026-01-11 22:28:06 -0500

    implements initial pannel

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,40 +1,32 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 
-interface ApiResponse {
-  items: {
-    id: number;
-    name: string;
-  }[]
-  meta: {
-    totalItems: number
-    itemCount: number
-    itemsPerPage: number
-    currentPage: number
-  }
-}
- const Page = async () => {
-  async function load(): Promise<ApiResponse> {
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(true)
-      }, 2000)
-    })
-    const responseApi = await fetch(
-      'https://dragonball-api.com/api/characters'
-    ).then((res) => res.json())
-    return responseApi
-  }
-
-  const response = await load()
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
   return (
-    <>
-      <p className="mb-5">Dashboard Page</p>
-      <ol>
-        {response.items.map((item: any) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ol>
-    </>
-  )
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        { <Card title="Collected" value={totalPaidInvoices} type="collected" /> }
+        { <Card title="Pending" value={totalPendingInvoices} type="pending" /> }
+        { <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> }
+        {<Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        {<RevenueChart revenue={revenue} />}
+        { <LatestInvoices latestInvoices={latestInvoices} /> }
+      </div>
+    </main>
+  );
 }
-
-export default Page
